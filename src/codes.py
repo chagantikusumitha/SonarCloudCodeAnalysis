978accecc9346ad38ff39d4463f32c4c19cf15a8
chaganti kusumitha <chagantikusumitha@chagantis-MacBook-Air.local>
2024-08-21 20:40:55 +0530

    duplicates

--- src/codes.py
+++ src/codes.py
@@ -5,3 +5,7 @@ def add_numbers(a, b):
 def multiply_numbers(a, b):
     """Returns the product of two numbers."""
     return a * b
+
+def multiply_numbers(a, b):
+    """Returns the product of two numbers."""
+    return a * b
---
+++ src/codes1.py
@@ -0,0 +1,3 @@
+def multiply_numbers(a, b):
+    """Returns the product of two numbers."""
+    return a * b
