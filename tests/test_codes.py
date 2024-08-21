264ad3180d4634b781a12616666d6c99d7ff8584
chagantikusumitha <92007497+chagantikusumitha@users.noreply.github.com>
2024-08-21 19:14:10 +0530

    Update test_codes.py

--- tests/test_codes.py
+++ tests/test_codes.py
@@ -5,11 +5,12 @@ import pytest
 from src.codes import add_numbers, multiply_numbers
 
 def test_add_numbers():
-    assert add_numbers(2, 3) == 6
+    assert add_numbers(2, 3) == 5
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
-
+    assert add_numbers(2, 2) == 4
 def test_multiply_numbers():
     assert multiply_numbers(2, 3) == 6
     assert multiply_numbers(-1, 1) == -1
     assert multiply_numbers(0, 5) == 0
+    assert multiply_numbers(0, 5) == 5
