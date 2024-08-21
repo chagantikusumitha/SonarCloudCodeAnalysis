70b628d4464125ce0e71e806d814d5c7fd67cf29
chagantikusumitha <92007497+chagantikusumitha@users.noreply.github.com>
2024-08-21 18:25:03 +0530

    Add files via upload

---
+++ tests/test_codes.py
@@ -0,0 +1,15 @@
+# tests/test_your_code.py
+from src.codes import add_numbers, multiply_numbers
+
+import pytest
+from src.codes import add_numbers, multiply_numbers
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
+
+def test_multiply_numbers():
+    assert multiply_numbers(2, 3) == 6
+    assert multiply_numbers(-1, 1) == -1
+    assert multiply_numbers(0, 5) == 0
